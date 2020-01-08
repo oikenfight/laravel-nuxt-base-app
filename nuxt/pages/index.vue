0d1925ff3d5f419d44581e6f2b274c9c65844869
oikenfight <oikenfight@gmail.com>
2020-01-08 22:25:24 +0900

    ready to index page

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,20 +1,50 @@
 <template>
-  <div>
-    Index
-  </div>
+  <v-container fluid class="grey lighten-4" style="height: 100%;">
+    <v-row v-if="!note">
+      open note !!
+    </v-row>
+    <v-row v-else>
+      <v-col cols="12">
+        <v-row align="start" justify="center">
+          <v-col cols="11">
+            <v-text-field :value="title" label="title" @change="inputTitle" />
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="12">
+        <v-row align="start" justify="center">
+          <v-col cols="11">
+            <div v-html="$md.render(input)"></div>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   layout: 'defualt',
   components: {},
+  data() {
+    return {
+      input: '# hello World!'
+    }
+  },
   computed: {
     ...mapGetters({
-      file: 'tree/note'
-    })
+      note: 'notes/note'
+    }),
+    title() {
+      return this.note.title
+    }
   },
-  mounted() {}
+  method: {
+    ...mapActions({
+      inputTitle: ''
+    })
+  }
 }
 </script>
