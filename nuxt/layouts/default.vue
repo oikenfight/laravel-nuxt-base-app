3ab6ed61f314136647e58d4cfcf76871e633b82e
oikenfight <oikenfight@gmail.com>
2020-01-26 14:29:03 +0900

    scaffold new default layout

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,128 +1,111 @@
 <template>
   <v-app id="">
-    <v-row class="fill-height" no-gutters>
-      <v-navigation-drawer
-        dark
-        mini-variant-width="50"
-        :mini-variant.sync="mini"
-        permanent
-      >
-        <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-          </v-list-item-avatar>
-          <v-list-item-title>Yuta Oikawa</v-list-item-title>
-          <v-btn icon @click.stop="mini = !mini">
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-        </v-list-item>
-
-        <v-divider></v-divider>
-
-        <v-list dense width="50" class="fill-height float-left">
-          <v-list-item-group>
-            <v-list-item v-for="menu in menues" :key="menu.title">
-              <v-list-item-icon>
-                <v-icon v-text="menu.icon"></v-icon>
-              </v-list-item-icon>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-
-        <v-divider vertical class="float-left"></v-divider>
-
-        <v-list class="grow">
-          <v-list-item v-for="link in links" :key="link" link>
-            <v-list-item-title v-text="link"></v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    </v-row>
-
-    <!-- <v-navigation-drawer
-      v-model="drawer"
-      app
-      mini-variant
+    <!-- Navigation -->
+    <v-navigation-drawer
       dark
-      color="teal"
-      :permanent="permanent"
-      :width="325"
+      app
+      mini-variant-width="50"
+      :mini-variant.sync="mini"
+      permanent
+      width="305"
     >
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Navigation lists
-            <v-btn
-              class="mx-2 float-right"
-              fab
-              small
-              outlined
-              color="grey lighten-2"
-            >
-              <v-icon dark>mdi-pencil</v-icon>
-            </v-btn>
-          </v-list-item-title>
-        </v-list-item-content>
+      <!-- Profile -->
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-title>Yuta Oikawa</v-list-item-title>
+        <v-btn icon @click.stop="mini = !mini">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
       </v-list-item>
-      <v-divider />
 
-      <v-row dense>
-        <v-col cols="6">
-          <v-list dense>
-            <v-list-item-group v-model="tree">
-              <v-row v-for="rack in racksAll" :key="rack.id">
-                <v-list-item @click="selectRack(rack)">
-                  <v-subheader>
-                    {{ rack.name }}
-                  </v-subheader>
-                  <v-btn
-                    class="float-right"
-                    fab
-                    text
-                    x-small
-                    color="grey lighten-2"
-                  >
-                    <v-icon class="float-right">mdi-dots-horizontal</v-icon>
-                  </v-btn>
-                </v-list-item>
-                <v-list-item
-                  v-for="folder in folders(rack.id)"
-                  :key="folder.id"
-                  @click="selectFolder(rack, folder)"
-                >
-                  <v-list-item-icon style="margin-right: 5px">
-                    <v-icon small v-text="folder.icon" />
-                  </v-list-item-icon>
-                  <v-list-item-title v-text="folder.name" />
-                </v-list-item>
-              </v-row>
-            </v-list-item-group>
-          </v-list>
-        </v-col>
-        <v-col cols="6">
-          <v-list dense>
-            <v-row v-for="note in notes(noteIds)" :key="note.id">
-              <v-card
-                class="mx-auto"
-                outlined
-                light
-                style="margin: 3px;"
-                @click="selectNote(note)"
-              >
-                <v-card-subtitle class="pb-0">2019/12/31</v-card-subtitle>
-                <v-card-text class="text--primary">
-                  <div>{{ note.title }}</div>
-                </v-card-text>
-              </v-card>
-            </v-row>
-          </v-list>
-        </v-col>
-      </v-row>
-    </v-navigation-drawer> -->
+      <v-divider></v-divider>
 
-    <v-content>
+      <!-- Side Menu（固定） -->
+      <v-list dense width="50" class="fill-height float-left">
+        <v-list-item-group>
+          <v-list-item v-for="menu in menues" :key="menu.title">
+            <v-list-item-icon>
+              <v-icon v-text="menu.icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+      <v-divider vertical inset class="float-left"></v-divider>
+
+      <!-- Racks/Folders -->
+      <v-list dense class="float-left pt-2" width="125" no-gutters>
+        <v-list-item-group v-model="tree">
+          <v-col v-for="rack in racksAll" :key="rack.id" class="ma-0 pa-0">
+            <v-list-item class="mt-1 pt-1 mx-0 px-0" @click="selectRack(rack)">
+              <v-subheader>
+                {{ rack.name }}
+              </v-subheader>
+              <v-spacer></v-spacer>
+              <v-btn text fab x-small>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </v-list-item>
+            <v-col class="my-0 py-0">
+              <v-divider></v-divider>
+            </v-col>
+            <v-list-item
+              v-for="folder in folders(rack.id)"
+              :key="folder.id"
+              class="ma-1 pa-1"
+              @click="selectFolder(rack, folder)"
+            >
+              <v-list-item-icon class="ma-1">
+                <v-icon small v-text="folder.icon" />
+              </v-list-item-icon>
+              <v-list-item-title v-text="folder.name" />
+            </v-list-item>
+          </v-col>
+        </v-list-item-group>
+      </v-list>
+
+      <v-divider vertical inset class="float-left"></v-divider>
+
+      <!-- Notes -->
+      <v-list dense class="float-left" width="125">
+        <v-row v-for="note in notes(noteIds)" :key="note.id">
+          <v-card
+            class="mx-auto"
+            outlined
+            light
+            style="margin: 3px;"
+            @click="selectNote(note)"
+          >
+            <v-card-subtitle class="pb-0">2019/12/31</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>{{ note.title }}</div>
+            </v-card-text>
+          </v-card>
+        </v-row>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Application Bar -->
+    <v-app-bar class="grey lighten-5" app>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+    </v-app-bar>
+
+    <!-- Content -->
+    <v-content class="grey lighten-5" app>
       <nuxt />
     </v-content>
+
+    <!-- Footer -->
+    <v-footer class="grey lighten-5" app inset>
+      <span class="">md-editor @oikawa</span>
+    </v-footer>
+    <!-- <v-footer class="grey lighten-5" app>
+      Vuetify
+    </v-footer> -->
+
+    <!-- Content -->
   </v-app>
 </template>
 
