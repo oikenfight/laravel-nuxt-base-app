04dadd87b9191da338b00fc1a0bddeeb304edd94
oikenfight <oikenfight@gmail.com>
2020-01-27 23:51:49 +0900

    use softDeletes

--- laravel/app/Entities/Entity.php
+++ laravel/app/Entities/Entity.php
@@ -2,9 +2,11 @@
 
 namespace App\Entities;
 
+use App\Entities\Contracts\EntityInterface;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Entity extends Model
+class Entity extends Model implements EntityInterface
 {
-    //
+  use SoftDeletes;
 }
