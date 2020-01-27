45a23d2ea49dae1568f180b2592c663f58ea8d84
oikenfight <oikenfight@gmail.com>
2020-01-27 22:49:11 +0900

    create tables: racks, folders, notes, items, releases and settings

---
+++ laravel/database/migrations/2020_01_27_131238_create_folders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoldersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('folders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->bigInteger('rack_id')->unsigned();
+            $table->foreign('rack_id')->references('id')->on('racks');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('folders');
+    }
+}
