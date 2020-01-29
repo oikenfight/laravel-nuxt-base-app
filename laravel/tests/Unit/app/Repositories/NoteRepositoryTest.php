abfe2e7bae26c9c461ce28b5a7e471be0736020b
oikenfight <oikenfight@gmail.com>
2020-01-29 20:27:53 +0900

    create repositories tests

---
+++ laravel/tests/Unit/app/Repositories/NoteRepositoryTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\app\Repositories;
+
+use PHPUnit\Framework\TestCase;
+
+final class NoteRepositoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
