6a0acef6f32e08e23b1734bac734cebff1257200
oikenfight <oikenfight@gmail.com>
2020-01-29 19:27:47 +0900

    create controllers

---
+++ laravel/app/Http/Controllers/Item/DestroyController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Item;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+final class DestroyController extends Controller
+{
+  public function __invoke()
+  {
+    //
+  }
+}
---
+++ laravel/app/Http/Controllers/Item/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Item;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+final class IndexController extends Controller
+{
+  public function __invoke()
+  {
+    //
+  }
+}
