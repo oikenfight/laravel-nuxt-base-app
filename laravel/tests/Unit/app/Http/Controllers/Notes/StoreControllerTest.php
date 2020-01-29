31e71b75c9b5b4ca89a17dcb475deb0ab0a7f3dd
oikenfight <oikenfight@gmail.com>
2020-01-29 19:50:39 +0900

    create controllers tests

---
+++ laravel/tests/Unit/app/Http/Controllers/Notes/StoreControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\app\Http\Controllers\Notes;
+
+use PHPUnit\Framework\TestCase;
+
+class StoreControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ laravel/tests/Unit/app/Http/Controllers/Racks/DestroyControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\app\Http\Controllers\Racks;
+
+use PHPUnit\Framework\TestCase;
+
+class DestroyControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
