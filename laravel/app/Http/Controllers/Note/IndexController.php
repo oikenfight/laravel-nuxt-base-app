84bc3602d572ee98d7376d8c8d776a12534d2b2a
oikenfight <oikenfight@gmail.com>
2020-02-04 23:42:16 +0900

    各テーブルの index api を作成（認証機能が未実装のため、user1 に紐づくデータを決め打ち）

--- laravel/app/Http/Controllers/Note/IndexController.php
+++ laravel/app/Http/Controllers/Note/IndexController.php
@@ -3,13 +3,34 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Note;
 
+use App\Entities\Contracts\NoteInterface;
+use App\Entities\Contracts\UserInterface;
+use App\Entities\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/**
+ * Class IndexController
+ * @package App\Http\Controllers\Note
+ */
 final class IndexController extends Controller
 {
-  public function __invoke()
-  {
-    //
-  }
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function __invoke()
+    {
+        // TODO: 認証ユーザを取得。ログイン機能未実装のため、暫定機に user1 とする
+        /** @var UserInterface $user */
+        $user = User::find(1);
+
+        // TODO: UseCase 作る
+        /** @var NoteInterface[] $notes */
+        $notes = $user->notes;
+        foreach ($notes as $key => $note) {
+            $note['itemsIds'] = $note->items->sortBy('order_index')->pluck('id');
+        }
+
+        return response()->json(['notes' => $notes]);
+    }
 }
