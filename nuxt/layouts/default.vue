d1c445ef67c02fc314d01425a1261a25cacdc75b
oikenfight <oikenfight@gmail.com>
2020-03-11 22:09:10 +0900

    app-bar を追加（後にタブで複数ノートを開けるようにしたい）

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,5 +1,40 @@
 <template>
   <v-app id="">
+    <v-app-bar color="" dark app>
+      <v-toolbar-title>Web Markdown Editor</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu bottom left>
+        <template v-slot:activator="{ on }">
+          <v-btn icon color="" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item @click="logout">
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item>
+          <v-list-item disabled>
+            <v-list-item-title>Setting</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+
     <!-- Navigation -->
     <v-navigation-drawer
       dark
@@ -15,6 +50,8 @@
           <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
         </v-list-item-avatar>
         <v-list-item-title>Yuta Oikawa</v-list-item-title>
+
+        <!-- ナビゲーションバー閉じるボタン -->
         <v-btn icon @click.stop="mini = !mini">
           <v-icon>mdi-chevron-left</v-icon>
         </v-btn>
@@ -25,7 +62,7 @@
       <!-- Side Menu（固定） -->
       <v-list dense width="50" class="fill-height float-left">
         <v-list-item-group>
-          <v-list-item v-for="menu in menus" :key="menu.name">
+          <v-list-item v-for="(menu, index) in menusSide" :key="index">
             <v-list-item-icon>
               <v-icon v-text="menu.icon"></v-icon>
             </v-list-item-icon>
@@ -133,7 +170,7 @@ export default {
     permanent: true,
     open: ['public'],
     tree: [],
-    menus: [
+    menusSide: [
       { name: 'Search', icon: 'search' },
       { name: 'Notes', icon: 'mdi-note-multiple' },
       { name: 'Releases', icon: 'mdi-folder-lock-open' }
@@ -175,6 +212,10 @@ export default {
     addNote() {
       this.$store.dispatch('notes/createNote') // ノートを作成し、notes/NoteId をセットする
       this.$store.dispatch('tree/addNoteId', this.noteId)
+    },
+    logout() {
+      this.$store.dispatch('logout')
+      window.location.href = '/login'
     }
   }
 }
