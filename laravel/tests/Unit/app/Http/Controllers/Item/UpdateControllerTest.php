dd93743a4f043e656735e66db605207301ebb255
oikenfight <oikenfight@gmail.com>
2020-04-29 01:00:45 +0900

    サーバ側の CRUD Item 機能のテストを追加

--- laravel/tests/Unit/app/Http/Controllers/Item/UpdateControllerTest.php
+++ laravel/tests/Unit/app/Http/Controllers/Item/UpdateControllerTest.php
@@ -3,17 +3,63 @@ declare(strict_types=1);
 
 namespace Tests\Unit\app\Http\Controllers\Item;
 
-use PHPUnit\Framework\TestCase;
+use App\Entities\Contracts\ItemInterface;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Item\UpdateController;
+use App\Http\UseCases\Contracts\Item\UpdateUseCaseInterface;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Mockery;
+use Tests\Unit\TestCase;
 
-class UpdateControllerTest extends TestCase
+final class UpdateControllerTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * test instance of.
      *
      * @return void
      */
-    public function testExample()
+    public function testInstanceOf()
     {
-        $this->assertTrue(true);
+        $controller = new UpdateController();
+
+        $this->assertInstanceOf(Controller::class, $controller);
+    }
+
+    /**
+     * test __invoke method
+     *
+     * @raturn void
+     */
+    public function testInvoke()
+    {
+        $itemId = 100;
+        $inputData = ['Dummy Item Data'];
+
+        /** @var Mockery\Mock|ItemInterface $item */
+        $item = Mockery::mock(ItemInterface::class);
+
+        /** @var Mockery\Mock|Request $request */
+        $request = Mockery::mock(Request::class);
+        $request->shouldReceive('route')->with('Item')->once()->andReturn($itemId);
+        $request->shouldReceive('get')->with('item')->once()->andReturn($inputData);
+
+        /** @var Mockery\Mock|UpdateUseCaseInterface $useCase */
+        $useCase = Mockery::mock(UpdateUseCaseInterface::class);
+        $useCase->shouldReceive('__invoke')->with($itemId, $inputData)->once()->andReturn($item);
+
+        /** @var Mockery\Mock|ResponseFactory $responseFactory */
+        $responseFactory = Mockery::mock(ResponseFactory::class);
+        $responseFactory->shouldReceive('json')->with([
+            'item' => $item
+        ])->once();
+
+        app()->bind(ResponseFactory::class, function () use ($responseFactory) {
+            return $responseFactory;
+        });
+
+        $controller = new UpdateController();
+
+        $controller($request, $useCase);
     }
 }
---
+++ laravel/tests/Unit/app/Http/UseCases/Item/StoreUseCaseTest.php
@@ -0,0 +1,118 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\app\Http\UseCases\Item;
+
+use App\Entities\Contracts\ItemInterface;
+use App\Entities\Contracts\UserInterface;
+use App\Http\UseCases\Contracts\Item\StoreUseCaseInterface;
+use App\Http\UseCases\Item\StoreUseCase;
+use App\Repositories\Contracts\ItemRepositoryInterface;
+use Mockery;
+use Tests\Unit\TestCase;
+
+
+/**
+ * Class StoreUseCaseTest
+ *
+ * @package Tests\Unit\app\Http\UseCases\Item
+ */
+final class StoreUseCaseTest extends TestCase
+{
+    /**
+     * test instance of
+     *
+     * @return void
+     */
+    public function testInstanceOf()
+    {
+        /** @var Mockery\Mock|ItemRepositoryInterface $repository */
+        $repository = Mockery::mock(ItemRepositoryInterface::class);
+
+        $useCase = new StoreUseCase($repository);
+
+        $this->assertInstanceOf(StoreUseCaseInterface::class, $useCase);
+    }
+
+    /**
+     * test __construct
+     *
+     * @return void
+     * @throws \ReflectionException
+     */
+    public function testConstruct()
+    {
+        /** @var Mockery\Mock|ItemRepositoryInterface $repository */
+        $repository = Mockery::mock(ItemRepositoryInterface::class);
+
+        $useCase = new StoreUseCase($repository);
+
+        $repositoryRef = $this->getHiddenProperty($useCase, 'repository');
+
+        $this->assertSame($repository, $repositoryRef->getValue($useCase));
+    }
+
+    /**
+     * test __invoke method
+     *
+     * @return void
+     */
+    public function testInvoke()
+    {
+        /** @var Mockery\Mock|ItemInterface $user */
+        $item = Mockery::mock(ItemInterface::class);
+        $expected = $item;
+
+        $userId = 100;
+        $noteId = 100;
+        $name = 'dummy item name';
+        $itemData = [
+            'name' => $name
+        ];
+        $createData = [
+            'user_id' => $userId,
+            'note_id' => $noteId,
+            'name' => $name,
+        ];
+
+        /** @var Mockery\Mock|ItemRepositoryInterface $repository */
+        $repository = Mockery::mock(ItemRepositoryInterface::class);
+        $repository->shouldReceive('create')->once()->with($createData)->andReturn($item);
+
+        $useCase = new StoreUseCase($repository);
+
+        $this->assertSame($expected, $useCase($userId, $noteId, $itemData));
+    }
+
+    /**
+     * test __invoke method with none input
+     *
+     * @return void
+     */
+    public function testInvokeWithItemDataIsNone()
+    {
+        /** @var Mockery\Mock|ItemInterface $user */
+        $item = Mockery::mock(ItemInterface::class);
+        $expected = $item;
+
+        $userId = 100;
+        $noteId = 100;
+        $createData = [
+            'user_id' => $userId,
+            'note_id' => $noteId,
+            'name' => null,
+        ];
+
+        /** @var Mockery\Mock|UserInterface $user */
+        $user = Mockery::mock(UserInterface::class);
+        $user->id = $userId;
+
+        /** @var Mockery\Mock|ItemRepositoryInterface $repository */
+        $repository = Mockery::mock(ItemRepositoryInterface::class);
+        $repository->shouldReceive('create')->once()->with($createData)->andReturn($item);
+
+        $useCase = new StoreUseCase($repository);
+
+        $this->assertSame($expected, $useCase($userId, $noteId));
+    }
+}
