44fa4ad8f81da602a00b0c081b83ac3e8dd59778
oikenfight <oikenfight@gmail.com>
2020-08-02 16:02:21 +0900

    fix it by refactoring NavigationDrawer.

--- nuxt/app/layouts/default.vue
+++ nuxt/app/layouts/default.vue
@@ -1,25 +1,5 @@
 <template>
   <v-app id="">
-    <!--    <v-app-bar app>-->
-    <!--      <v-row>-->
-    <!--        <v-col cols class="col-9">-->
-    <!--          <v-toolbar-title>Web Markdown Editor</v-toolbar-title>-->
-    <!--        </v-col>-->
-    <!--        <v-col cols class="col-3">-->
-    <!--          <AppBarMenu></AppBarMenu>-->
-    <!--        </v-col>-->
-    <!--      </v-row>-->
-
-    <!--      &lt;!&ndash; タブ機能はそのうち付けたい &ndash;&gt;-->
-    <!--      &lt;!&ndash;      <template v-slot:extension>&ndash;&gt;-->
-    <!--      &lt;!&ndash;        <v-tabs align-with-title>&ndash;&gt;-->
-    <!--      &lt;!&ndash;          <v-tab>Tab 1</v-tab>&ndash;&gt;-->
-    <!--      &lt;!&ndash;          <v-tab>Tab 2</v-tab>&ndash;&gt;-->
-    <!--      &lt;!&ndash;          <v-tab>Tab 3</v-tab>&ndash;&gt;-->
-    <!--      &lt;!&ndash;        </v-tabs>&ndash;&gt;-->
-    <!--      &lt;!&ndash;      </template>&ndash;&gt;-->
-    <!--    </v-app-bar>-->
-
     <!-- Navigation -->
     <v-navigation-drawer
       app
@@ -47,7 +27,7 @@
       <v-divider></v-divider>
 
       <!-- Navigation のメインコンテンツ -->
-      <NavigationController></NavigationController>
+      <NavigationBase></NavigationBase>
     </v-navigation-drawer>
 
     <!-- Application Bar -->
@@ -71,20 +51,13 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-// import AppBarMenu from '@/components/appbar/Menu.vue'
-// import AppBarTabs from '@/components/appbar/Tabs.vue'
-import NavigationController from '@/components/MyPage/NavigationDrawer/NavigationController'
+import NavigationBase from '@/components/MyPage/NavigationDrawer/NavigationBase'
 
 export default {
   middleware: 'auth',
-  components: { NavigationController },
+  components: { NavigationBase },
   data: () => ({
-    mini: true,
-    menuTogglable: {
-      ListSearch: false,
-      ListWork: true,
-      ListHome: false
-    }
+    mini: true
   }),
   computed: {
     ...mapGetters({
@@ -92,15 +65,7 @@ export default {
     })
   },
   methods: {
-    ...mapActions({}),
-    toggleMenu({ menuName }) {
-      Object.keys(this.menuTogglable).forEach((key) => {
-        this.menuTogglable[key] = key === menuName
-      })
-    },
-    shouldShow(menuName) {
-      return this.menuTogglable[menuName]
-    }
+    ...mapActions({})
   }
 }
 </script>
